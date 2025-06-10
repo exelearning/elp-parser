04dff48defdaabb1d89219bb5f3855b806a84e37
Ernesto Serrano <info@ernesto.es>
2025-06-10 12:44:55 +0100

    Normalize filenames with accent removal

--- src/ElpParser.php
+++ src/ElpParser.php
@@ -722,7 +722,7 @@ class ELPParser implements \JsonSerializable
      */
     protected function slug(string $text): string
     {
-        $slug = iconv('UTF-8', 'ASCII//TRANSLIT', $text);
+        $slug = remove_accents($text);
         $slug = strtolower($slug);
         $slug = preg_replace('/[^a-z0-9]+/', '_', $slug);
         return trim($slug, '_');
@@ -751,4 +751,142 @@ class ELPParser implements \JsonSerializable
         $zip->extractTo($destinationPath);
         $zip->close();
     }
+}
+
+/**
+ * Remove accents from a string using WordPress\' implementation.
+ *
+ * This function is copied from WordPress 6.8.1 and retains its original
+ * copyright notice.
+ *
+ * @author  WordPress contributors
+ * @license GPL-2.0-or-later
+ *
+ * @param string $text   Text that might have accent characters.
+ * @param string $locale Optional. The locale to use for accent removal.
+ * @return string Filtered string with replaced characters.
+ *
+ * @see https://github.com/WordPress/wordpress-develop/blob/6.8.1/src/wp-includes/formatting.php
+ */
+function remove_accents(string $text, string $locale = ''): string
+{
+    if (!preg_match('/[\x80-\xff]/', $text)) {
+        return $text;
+    }
+
+    if (seems_utf8($text)) {
+        if (function_exists('normalizer_is_normalized') && function_exists('normalizer_normalize')) {
+            if (!normalizer_is_normalized($text)) {
+                $text = normalizer_normalize($text);
+            }
+        }
+
+        $chars = [
+            'ª' => 'a', 'º' => 'o', 'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A',
+            'Ä' => 'A', 'Å' => 'A', 'Æ' => 'AE', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
+            'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+            'Ð' => 'D', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O',
+            'Ö' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y',
+            'Þ' => 'TH', 'ß' => 's', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a',
+            'ä' => 'a', 'å' => 'a', 'æ' => 'ae', 'ç' => 'c', 'è' => 'e', 'é' => 'e',
+            'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+            'ð' => 'd', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o',
+            'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u',
+            'ý' => 'y', 'þ' => 'th', 'ÿ' => 'y', 'Ø' => 'O', 'Ā' => 'A', 'ā' => 'a',
+            'Ă' => 'A', 'ă' => 'a', 'Ą' => 'A', 'ą' => 'a', 'Ć' => 'C', 'ć' => 'c',
+            'Ĉ' => 'C', 'ĉ' => 'c', 'Ċ' => 'C', 'ċ' => 'c', 'Č' => 'C', 'č' => 'c',
+            'Ď' => 'D', 'ď' => 'd', 'Đ' => 'D', 'đ' => 'd', 'Ē' => 'E', 'ē' => 'e',
+            'Ĕ' => 'E', 'ĕ' => 'e', 'Ė' => 'E', 'ė' => 'e', 'Ę' => 'E', 'ę' => 'e',
+            'Ě' => 'E', 'ě' => 'e', 'Ĝ' => 'G', 'ĝ' => 'g', 'Ğ' => 'G', 'ğ' => 'g',
+            'Ġ' => 'G', 'ġ' => 'g', 'Ģ' => 'G', 'ģ' => 'g', 'Ĥ' => 'H', 'ĥ' => 'h',
+            'Ħ' => 'H', 'ħ' => 'h', 'Ĩ' => 'I', 'ĩ' => 'i', 'Ī' => 'I', 'ī' => 'i',
+            'Ĭ' => 'I', 'ĭ' => 'i', 'Į' => 'I', 'į' => 'i', 'İ' => 'I', 'ı' => 'i',
+            'Ĳ' => 'IJ','ĳ' => 'ij','Ĵ' => 'J', 'ĵ' => 'j', 'Ķ' => 'K', 'ķ' => 'k',
+            'ĸ' => 'k', 'Ĺ' => 'L', 'ĺ' => 'l', 'Ļ' => 'L', 'ļ' => 'l', 'Ľ' => 'L',
+            'ľ' => 'l', 'Ŀ' => 'L', 'ŀ' => 'l', 'Ł' => 'L', 'ł' => 'l', 'Ń' => 'N',
+            'ń' => 'n', 'Ņ' => 'N', 'ņ' => 'n', 'Ň' => 'N', 'ň' => 'n', 'ŉ' => 'n',
+            'Ŋ' => 'N', 'ŋ' => 'n', 'Ō' => 'O', 'ō' => 'o', 'Ŏ' => 'O', 'ŏ' => 'o',
+            'Ő' => 'O', 'ő' => 'o', 'Œ' => 'OE','œ' => 'oe','Ŕ' => 'R', 'ŕ' => 'r',
+            'Ŗ' => 'R', 'ŗ' => 'r', 'Ř' => 'R', 'ř' => 'r', 'Ś' => 'S', 'ś' => 's',
+            'Ŝ' => 'S', 'ŝ' => 's', 'Ş' => 'S', 'ş' => 's', 'Š' => 'S', 'š' => 's',
+            'Ţ' => 'T', 'ţ' => 't', 'Ť' => 'T', 'ť' => 't', 'Ŧ' => 'T', 'ŧ' => 't',
+            'Ũ' => 'U', 'ũ' => 'u', 'Ū' => 'U', 'ū' => 'u', 'Ŭ' => 'U', 'ŭ' => 'u',
+            'Ů' => 'U', 'ů' => 'u', 'Ű' => 'U', 'ű' => 'u', 'Ų' => 'U', 'ų' => 'u',
+            'Ŵ' => 'W', 'ŵ' => 'w', 'Ŷ' => 'Y', 'ŷ' => 'y', 'Ÿ' => 'Y', 'Ź' => 'Z',
+            'ź' => 'z', 'Ż' => 'Z', 'ż' => 'z', 'Ž' => 'Z', 'ž' => 'z', 'ſ' => 's',
+            'Ə' => 'E', 'ǝ' => 'e', 'Ș' => 'S', 'ș' => 's', 'Ț' => 'T', 'ț' => 't',
+            '€' => 'E', '£' => '', 'Ơ' => 'O', 'ơ' => 'o', 'Ư' => 'U', 'ư' => 'u',
+            'Ầ' => 'A', 'ầ' => 'a', 'Ằ' => 'A', 'ằ' => 'a', 'Ề' => 'E', 'ề' => 'e',
+            'Ồ' => 'O', 'ồ' => 'o', 'Ờ' => 'O', 'ờ' => 'o', 'Ừ' => 'U', 'ừ' => 'u',
+            'Ỳ' => 'Y', 'ỳ' => 'y', 'Ả' => 'A', 'ả' => 'a', 'Ẩ' => 'A', 'ẩ' => 'a',
+            'Ẳ' => 'A', 'ẳ' => 'a', 'Ể' => 'E', 'ể' => 'e', 'Ỏ' => 'O', 'ỏ' => 'o',
+            'Ổ' => 'O', 'ổ' => 'o', 'Ở' => 'O', 'ở' => 'o', 'Ủ' => 'U', 'ủ' => 'u',
+            'Ử' => 'U', 'ử' => 'u', 'Ỷ' => 'Y', 'ỷ' => 'y', 'Ẫ' => 'A', 'ẫ' => 'a',
+            'Ậ' => 'A', 'ậ' => 'a', 'Ắ' => 'A', 'ắ' => 'a', 'Ế' => 'E', 'ế' => 'e',
+            'Ố' => 'O', 'ố' => 'o', 'Ớ' => 'O', 'ớ' => 'o', 'Ứ' => 'U', 'ứ' => 'u',
+            'Ạ' => 'A', 'ạ' => 'a', 'Ậ' => 'A', 'ậ' => 'a', 'Ặ' => 'A', 'ặ' => 'a',
+            'Ẹ' => 'E', 'ẹ' => 'e', 'Ệ' => 'E', 'ệ' => 'e', 'Ỉ' => 'I', 'ỉ' => 'i',
+            'Ị' => 'I', 'ị' => 'i', 'Ọ' => 'O', 'ọ' => 'o', 'Ợ' => 'O', 'ợ' => 'o',
+            'Ụ' => 'U', 'ụ' => 'u', 'Ỵ' => 'Y', 'ỵ' => 'y', 'Ỹ' => 'Y', 'ỹ' => 'y',
+            'Ấ' => 'A', 'ấ' => 'a', 'Ắ' => 'A', 'ắ' => 'a', 'Ế' => 'E', 'ế' => 'e',
+            'Ố' => 'O', 'ố' => 'o', 'Ớ' => 'O', 'ớ' => 'o', 'Ứ' => 'U', 'ứ' => 'u',
+        ];
+
+        if ('de_DE' === $locale || 'de_DE_formal' === $locale ||
+            'de_CH' === $locale || 'de_CH_informal' === $locale ||
+            'de_AT' === $locale) {
+            $chars['Ä'] = 'Ae';
+            $chars['ä'] = 'ae';
+            $chars['Ö'] = 'Oe';
+            $chars['ö'] = 'oe';
+            $chars['Ü'] = 'Ue';
+            $chars['ü'] = 'ue';
+            $chars['ß'] = 'ss';
+        } elseif ('da_DK' === $locale) {
+            $chars['Æ'] = 'Ae';
+            $chars['æ'] = 'ae';
+            $chars['Ø'] = 'Oe';
+            $chars['ø'] = 'oe';
+            $chars['Å'] = 'Aa';
+            $chars['å'] = 'aa';
+        } elseif ('ca' === $locale) {
+            $chars['l·l'] = 'll';
+        } elseif ('sr_RS' === $locale || 'bs_BA' === $locale) {
+            $chars['Đ'] = 'DJ';
+            $chars['đ'] = 'dj';
+        }
+
+        return strtr($text, $chars);
+    }
+
+    $chars = [];
+    $chars['in'] = "\x80\x83\x8a\x8e\x9a\x9e"
+        . "\x9f\xa2\xa5\xb5\xc0\xc1\xc2"
+        . "\xc3\xc4\xc5\xc7\xc8\xc9\xca"
+        . "\xcb\xcc\xcd\xce\xcf\xd1\xd2"
+        . "\xd3\xd4\xd5\xd6\xd8\xd9\xda"
+        . "\xdb\xdc\xdd\xe0\xe1\xe2\xe3"
+        . "\xe4\xe5\xe7\xe8\xe9\xea\xeb"
+        . "\xec\xed\xee\xef\xf1\xf2\xf3"
+        . "\xf4\xf5\xf6\xf8\xf9\xfa\xfb"
+        . "\xfc\xfd\xff";
+
+    $chars['out'] = 'EfSZszYcYuAAAAAACEEEEIIIINOOOOOOUUUUYaaaaaaceeeeiiiinoooooouuuuyy';
+
+    $text = strtr($text, $chars['in'], $chars['out']);
+
+    $double_chars['in']  = ["\x8c", "\x9c", "\xc6", "\xd0", "\xde", "\xdf", "\xe6", "\xf0", "\xfe"];
+    $double_chars['out'] = ['OE', 'oe', 'AE', 'DH', 'TH', 'ss', 'ae', 'dh', 'th'];
+    return str_replace($double_chars['in'], $double_chars['out'], $text);
+}
+
+/**
+ * Determine if a string is valid UTF-8.
+ *
+ * @param string $str Input string.
+ * @return bool True if the string is valid UTF-8.
+ */
+function seems_utf8(string $str): bool
+{
+    return mb_detect_encoding($str, 'UTF-8', true) !== false;
 }
